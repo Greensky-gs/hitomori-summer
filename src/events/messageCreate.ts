077965d480a128e00396a8d17e6fa831685cd979
Greensky <david.hesliere@gmail.com>
2023-06-23 16:29:47 +0200

    chore(event): :zap: Make points only in 2 categories

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,6 +1,12 @@
 import { AmethystEvent } from 'amethystjs';
 import { database } from '../utils/db';
+import { GuildTextBasedChannel } from 'discord.js';
 
 export default new AmethystEvent('messageCreate', async (message) => {
-    if (message.guild && !message.author.bot) database.addUser(message.author.id);
+    if (
+        message.guild &&
+        !message.author.bot &&
+        ['715281992199176274', '1035488039549546506'].includes((message.channel as GuildTextBasedChannel)?.parent?.id)
+    )
+        database.addUser(message.author.id);
 });
