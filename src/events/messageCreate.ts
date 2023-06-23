61e9a68a72ac43a9eaa0b3dbf752b401949a5d41
Greensky-gs <72513011+Greensky-gs@users.noreply.github.com>
2023-06-23 17:19:35 +0200

    fix: :bug: Ignore some other channels

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -6,7 +6,8 @@ export default new AmethystEvent('messageCreate', async (message) => {
     if (
         message.guild &&
         !message.author.bot &&
-        ['715281992199176274', '1035488039549546506'].includes((message.channel as GuildTextBasedChannel)?.parent?.id)
+        ['715281992199176274', '1035488039549546506'].includes((message.channel as GuildTextBasedChannel)?.parent?.id) &&
+        !['1007217195585122365', '810196285427941386', '996694895119061053'].includes(message.channel.id)
     )
         database.addUser(message.author.id);
 });
