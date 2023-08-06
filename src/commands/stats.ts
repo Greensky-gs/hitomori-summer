86c8b7d3c56be009b5295117344fa73a706716e6
Greensky <david.hesliere@gmail.com>
2023-08-06 16:00:53 +0200

    feat(cmd): :sparkles: Add ways to get a user's stats

--- src/commands/stats.ts
+++ src/commands/stats.ts
@@ -4,18 +4,35 @@ import { classic } from '../utils/embeds';
 import { emojisData } from '../data/emojis';
 import { statuses } from '../data/statuses';
 import { displayBanner } from '../utils/banner';
+import { GuildMember } from 'discord.js';
 
 export default new AmethystCommand({
     name: 'stats',
     description: 'Affiche vos stats',
     preconditions: [preconditions.GuildOnly]
 }).setMessageRun(async ({ message }) => {
-    const stats = database.getUser(message.author.id);
+    const userId =
+        message.mentions.users?.first()?.id ??
+        (/\d{16,}/.test(message.content)
+            ? ((await message.guild.members.fetch(message.content)?.catch(log4js.trace)) as GuildMember)?.id
+            : null) ??
+        message.guild.members.cache.get(message?.content)?.id ??
+        message.guild.members.cache.find(
+            (x) =>
+                x.user.username.toLowerCase() === message.content?.toLowerCase() ||
+                (x.nickname && x.nickname?.toLowerCase() === message.content?.toLowerCase())
+        )?.id ??
+        message.author.id;
+    const stats = database.getUser(userId);
 
     const banner = displayBanner();
     const embed = classic(message.author)
         .setTitle('Statistiques')
-        .setDescription(`Vos statistiques pour l'évènement d'été :`)
+        .setDescription(
+            `${
+                userId === message.author.id ? 'Vos statistiques' : `Les statistiques de <@${userId}>`
+            } pour l'évènement d'été :`
+        )
         .setColor(message.member.displayHexColor)
         .setFields(
             {
